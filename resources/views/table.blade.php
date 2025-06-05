4c20542ee25b35cd5137dcc98c749b375ea057b0
galuhrabbany <galuhrabbany@gmail.com>
2025-06-05 15:57:17 +0700

    Acara 12 DataTables

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -1,32 +1,130 @@
 @extends('layout.template')
 
 @section('content')
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Description</th>
-                <th>Image</th>
-                <th>Created At</th>
-                <th>Updated At</th>
-            </tr>
-        </thead>
+<div class="container mt-4">
+    <div class="card shadow-sm">
+        <div class="card-header">
+            <h5 class="mb-0">Data Points</h5>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped" id="pointsTable">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Description</th>
+                            <th>Image</th>
+                            <th>Created At</th>
+                            <th>Updated At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($points as $p)
+                            <tr>
+                                <td>{{ $p->id }}</td>
+                                <td>{{ $p->name }}</td>
+                                <td>{{ $p->description }}</td>
+                                <td>
+                                    <img src="{{ asset('storage/images/' . $p->image)}}" alt=""
+                                    width="120" class="img-thumbnail" title="{{ $p->image }}">
+                                </td>
+                                <td>{{ $p->created_at }}</td>
+                                <td>{{ $p->updated_at }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
-        <tbody>
-            @foreach ($points as $p)
-                <tr>
-                    <td>{{ $p->id }}</td>
-                    <td>{{ $p->name }}</td>
-                    <td>{{ $p->description }}</td>
-                    <td>
-                        <img src="{{ asset('storage/images/' . $p->image)}}" alt=""
-                        width="200" title="{{$p->image}}">
-                    </td>
-                    <td>{{ $p->created_at }}</td>
-                    <td>{{ $p->updated_at }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="card shadow-sm mt-4">
+        <div class="card-header">
+            <h5 class="mb-0">Data Polylines</h5>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped" id="polylinesTable">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Description</th>
+                            <th>Image</th>
+                            <th>Created At</th>
+                            <th>Updated At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($polylines as $p)
+                            <tr>
+                                <td>{{ $p->id }}</td>
+                                <td>{{ $p->name }}</td>
+                                <td>{{ $p->description }}</td>
+                                <td>
+                                    <img src="{{ asset('storage/images/' . $p->image)}}" alt=""
+                                    width="120" class="img-thumbnail" title="{{ $p->image }}">
+                                </td>
+                                <td>{{ $p->created_at }}</td>
+                                <td>{{ $p->updated_at }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="card shadow-sm mt-4">
+        <div class="card-header">
+            <h5 class="mb-0">Data Polygons</h5>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped" id="polygonsTable">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Description</th>
+                            <th>Image</th>
+                            <th>Created At</th>
+                            <th>Updated At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($polygons as $p)
+                            <tr>
+                                <td>{{ $p->id }}</td>
+                                <td>{{ $p->name }}</td>
+                                <td>{{ $p->description }}</td>
+                                <td>
+                                    <img src="{{ asset('storage/images/' . $p->image)}}" alt=""
+                                    width="120" class="img-thumbnail" title="{{ $p->image }}">
+                                </td>
+                                <td>{{ $p->created_at }}</td>
+                                <td>{{ $p->updated_at }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('styles')
+<link rel="stylesheet" href="https://cdn.datatables.net/2.3.1/css/dataTables.dataTables.min.css">
+@endsection
+
+@section('scripts')
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/2.3.1/js/dataTables.min.js"></script>
+<script>
+    let tablepoints = new DataTable('#pointsTable');
+    let tablepolylines = new DataTable('#polylinesTable');
+    let tablepolygons = new DataTable('#polygonsTable');
+</script>
 @endsection
